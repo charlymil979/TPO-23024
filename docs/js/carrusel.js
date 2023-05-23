c9bd87f3cfa8327fc787542dd1e67a69d5585cea
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2023-05-22 22:10:15 -0300

    Add files via upload

---
+++ docs/js/carrusel.js
@@ -0,0 +1,33 @@
+const d = document;
+const $carrusel = d.querySelector(".carrusel");
+
+function carrusel() {
+  fetch("/json/carrusel.json")
+    .then((resp) => resp.json())
+    .then((data) => {
+      // console.log(data);
+      let $imagenes = "";
+      for (const foto in data) {
+        // console.log(foto, data[foto]);
+        $imagenes += `
+      <img class="carrusel-foto" src="${data[foto]}" alt="${foto}">
+      `;
+      }
+      console.log($imagenes)
+      $carrusel.innerHTML = $imagenes;
+      // console.log($carrusel);
+      const $foto = d.querySelectorAll(".carrusel-foto");
+      // console.log($foto)
+      let posic=0
+      let maxPosic=$foto.length
+      setInterval(() => {
+        $foto.forEach(foto =>{
+          foto.classList.remove("foto-activa");
+        })
+        $foto[posic].classList.add("foto-activa")
+        posic++
+        if(posic===maxPosic) {posic=0};
+      }, 2000);
+    });
+}
+carrusel();
