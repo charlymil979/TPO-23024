219f64bf28c019155b986ab49e452402bbe5a637
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2023-05-27 20:26:28 -0300

    Add files via upload

--- docs/js/dolar.js
+++ docs/js/dolar.js
@@ -6,14 +6,23 @@ const $mostrar = d.querySelector("#imgdolar")
 
 function dolar() {
   d.addEventListener('click', e=>{
-    console.log(e.target)
+    // console.log(e.target)
     if(e.target.matches(".cierre")){
       e.preventDefault();
-      $dolar.classList.add('none')
+      document.body.removeChild($iframe)
     }
     if (e.target.matches("#imgdolar")) {
       e.preventDefault();
-      $dolar.classList.remove("none");
+      $iframe=document.createElement("div")
+      $iframe.classList.add("dolar")
+      $iframe.innerHTML = `
+      <button class="cierre">x</button>
+      <iframe
+        style="width:320px;height:260px;border-radius:10px;box-shadow:2px 4px 4px rgb(0 0 0 / 25%);display:flex;justify-content:center;border:1px solid #bcbcbc"
+        src="https://dolarhoy.com/i/cotizaciones/dolar-bancos-y-casas-de-cambio" frameborder="0"></iframe>
+      <p>Los precios son a valor dolar oficial</p>
+    `;
+      document.body.appendChild($iframe);
     }
   })
 }
